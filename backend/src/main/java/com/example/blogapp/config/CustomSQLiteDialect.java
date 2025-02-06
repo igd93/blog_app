85b26e0281ce1aa327ea4a7ac4f3642b09736aa3
igd93 <102153262+igd93@users.noreply.github.com>
2025-02-06 16:53:18 +0100

    Deleted unnecessary sql dialect

--- backend/src/main/java/com/example/blogapp/config/CustomSQLiteDialect.java
+++
@@ -1,9 +0,0 @@
-package com.example.blogapp.config;
-
-import org.hibernate.dialect.SQLiteDialect;
-
-public class CustomSQLiteDialect extends SQLiteDialect {
-    public CustomSQLiteDialect() {
-        super();
-    }
-}
