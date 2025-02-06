9fa0e059cecab0fcb55bde00737a99aac324d4be
igd93 <102153262+igd93@users.noreply.github.com>
2025-02-06 13:13:00 +0100

    Removed legacyBehavior

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -56,7 +56,7 @@ export default function Header() {
             <NavigationMenu>
               <NavigationMenuList>
                 <NavigationMenuItem>
-                  <Link to="/" legacyBehavior passHref>
+                  <Link to="/">
                     <NavigationMenuLink
                       className={navigationMenuTriggerStyle()}
                     >
@@ -65,7 +65,7 @@ export default function Header() {
                   </Link>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                  <Link to="/about" legacyBehavior passHref>
+                  <Link to="/about">
                     <NavigationMenuLink
                       className={navigationMenuTriggerStyle()}
                     >
